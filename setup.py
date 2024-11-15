4fef70664e3f44007df8b9bc88a17f17efa5dd35
Subil <subiljacob@gmail.com>
2024-11-14 23:52:58 -0700

    EDA and model training

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages # type: ignore
+
+setup(
+    name="porosity_pred",
+    version="0.0.0",
+    author="subil",
+    author_email="subiljacob@gmail.com",
+    packages=find_packages()
+)
